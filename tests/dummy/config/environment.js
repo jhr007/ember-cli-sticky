88a61276fa43b93c8a9d6ee71ffb8e0f261e781b
John Robinson <john.holland.robinson@gmail.com>
2015-06-25 16:42:14 -0500

    Changed baseURL in dummy/config/environment.js

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/ember-cli-sticky',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -29,7 +29,7 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
+    ENV.baseURL = '/ember-cli-sticky';
     ENV.locationType = 'none';
 
     // keep test console output quieter
